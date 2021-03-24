b00a47645a9ce56bac34857105dccc8ef4a8c6a7
Matthew Sills <matthew@eightminuteswest.com>
2021-03-24 17:05:01 +0100

    modify build script

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+rm -f dist/*
 python setup.py sdist bdist_wheel
 tar tzf dist/*.tar.gz
 twine check dist/*
 
 # upload to testpypi configured in ~/.pypirc
-twine upload --repository testpypi dist/*
+# twine upload --repository testpypi dist/*
 # or: twine upload --repository-url https://test.pypi.org/legacy/ dist/*
 # to test install: pip install --index-url https://test.pypi.org/simple/ --no-deps emw_serializer
 
 # upload to pypi configured in ~/.pypirc
-# twine upload --repository pypi dist/*
+twine upload --repository pypi dist/*
